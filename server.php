0f09a7f5331722f779036849ebeb2f017ae579b4
Aligern <sasso.luca93@gmail.com>
2024-05-06 17:17:11 +0200

    try file_put_contents

--- server.php
+++ server.php
@@ -1,16 +1,26 @@
 <?php
 
-var_dump(file_get_contents('data.json'));
-// file_get_contents reads the content of a file
-// file_put_contents writes content to a file
+// file_get_contents reads the content of a file 
+// var_dump(file_get_contents('data.json'));
 
+$dataJson = file_get_contents('/data.json'); 
 
+$data = json_decode($dataJson, true);
+var_dump($data);
+
+// file_put_contents writes content to a file (1st parameter is the path, 2nd is the content)
+
+$tryData = [
+    'name' => 'Pippo',
+    'surname' => 'Pluto',
+    'age' => 5,
+    'alive' => true
+];
 // json_decode returns an associative array or an object depending on the type of data in the file content
+file_put_contents('prova.json', json_encode($tryData));
 
-$shipJson =  file_get_contents("js/data.json");
+
 // var_dump($shipJson);
 
 
 
-header("Content-Type: application/json");
-echo $shipJson;
